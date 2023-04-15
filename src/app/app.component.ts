5256b48885c54d8d141c395262168a29f7f3d8dc
gailav <gaila.vastakaite@gmail.com>
2023-04-15 22:24:28 +0300

    fix: unput validations for single applicant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, } from '@angular/core';
+import { Component } from '@angular/core';
 import { UserService } from './user.service';
-
 
 @Component({
   selector: 'app-root',
--- src/app/max-loan-calculator/max-loan-calculator.component.ts
+++ src/app/max-loan-calculator/max-loan-calculator.component.ts
@@ -1,39 +1,35 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder, ValidationErrors, AbstractControl, ValidatorFn } from '@angular/forms';
 
-
 const negativeValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
   const netIncome = control.value;
   return netIncome && netIncome < 0 ? { negativeNetIncome: true } : null;
 }
-const applicantNetIncomeCheck: ValidatorFn = (FormControl) =>{
-  if(FormControl.value <600){
-    return {applicantNetIncomeCheck: true};
-  }
-  return null;
+
+const applicantNetIncomeCheck: ValidatorFn = (formControl) => {
+  if (formControl.value < 600) {
+    return { applicantNetIncomeCheck: true }
+  } return null
 }
 
-const applicantWithDependentsCheck: ValidatorFn = (control) =>{
+const applicantWithDependentsCheck: ValidatorFn = (control) => {
   const netIncome = control.get('netIncome')?.value;
   const dependents = control.get('dependent')?.value;
-  
-    if( dependents >=2 && netIncome < 1000 ){
-      return {applicantWithMoreThanTwoDependentsError: true};
-    } else if (dependents === 1 && netIncome < 650 ){
-      return {applicantWithOneDependentError: true};
-}else {
-  return null
-}
-}
 
-
+  if (dependents >= 2 && netIncome < 1000) {
+    return { applicantWithMoreThanTwoDependentsError: true };
+  } else if (dependents === 1 && netIncome < 650) {
+    return { applicantWithOneDependentError: true };
+  } else {
+    return null
+  }
+}
 
 @Component({
   selector: 'app-max-loan-calculator',
   templateUrl: './max-loan-calculator.component.html',
   styleUrls: ['./max-loan-calculator.component.css']
 })
-
 
 export class MaxLoanCalculatorComponent {
   loanForm: FormGroup;
@@ -42,8 +38,9 @@ export class MaxLoanCalculatorComponent {
     this.loanForm = this.fb.group({
       netIncome: new FormControl('', [Validators.required, Validators.pattern(/^\d+$/), negativeValidator, applicantNetIncomeCheck]),
       obligations: new FormControl('', [Validators.required, Validators.pattern(/^\d+$/)]),
-      dependent: new FormControl('', [Validators.required, Validators.pattern(/^\d+$/), negativeValidator])},
-      {validators: [applicantWithDependentsCheck]}
+      dependent: new FormControl('', [Validators.required, Validators.pattern(/^\d+$/), negativeValidator])
+    },
+      { validators: [applicantWithDependentsCheck] }
     );
   }
   get netIncome() {
@@ -56,6 +53,5 @@ export class MaxLoanCalculatorComponent {
   get dependent() {
     return this.loanForm.get('dependent');
   }
-
 }
 
