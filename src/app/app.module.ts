17f3506df38e6358db70a0fa8e2b81a2fbca2c6f
[Valdemaras] <[valdemarlamm@gmail.com]>
2023-04-15 16:45:14 +0300

    feat: morgApply form with basic validation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
@@ -9,7 +8,16 @@ import { HeaderComponent } from './header/header.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatButtonModule} from '@angular/material/button';
-
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { HttpClientModule } from '@angular/common/http';
 import { FirstTabComponent } from './first-tab/first-tab.component';
 import { SecondTabComponent } from './second-tab/second-tab.component';
@@ -17,12 +25,22 @@ import { ThirdTabComponent } from './third-tab/third-tab.component';
 
 @NgModule({
   imports: [
+    MatTooltipModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatTabsModule,
     MatButtonModule,
-    HttpClientModule
+    HttpClientModule,
+    MatStepperModule,
+    MatFormFieldModule
   ],
   declarations: [
     AppComponent,
@@ -32,7 +50,6 @@ import { ThirdTabComponent } from './third-tab/third-tab.component';
     FirstTabComponent,
     SecondTabComponent,
     ThirdTabComponent,
-
   ],
 
   providers: [],
