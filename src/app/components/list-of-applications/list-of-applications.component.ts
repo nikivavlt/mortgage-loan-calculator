347001b5bef5b3767d87bc2d689b8cb731b06777
gailav <gaila.vastakaite@gmail.com>
2023-04-19 16:10:51 +0300

    feat:add all applications list

--- src/app/components/list-of-applications/list-of-applications.component.ts
+++ src/app/components/list-of-applications/list-of-applications.component.ts
@@ -1,8 +1,10 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { Observable, Subject, catchError, map, of, takeUntil, tap } from 'rxjs';
 import { AllApplications } from 'src/app/interfaces/all-applications-list';
 import { GetAllApplicationsService } from 'src/app/services/get-all-applications.service';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-list-of-applications',
@@ -12,30 +14,42 @@ import { GetAllApplicationsService } from 'src/app/services/get-all-applications
 export class ListOfApplicationsComponent {
   displayedColumns: string[] = ['date', 'id', 'firstName', 'lastName'];
   dataSource = new MatTableDataSource<AllApplications>;
-
+  private readonly destroy$ = new Subject<void>();
+  
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  dialog: any;
   
   constructor(private getAllApplicationsService: GetAllApplicationsService) {}
 
+
+  ngOnInit() {
+    this.getAllApplicationsService.getAllApplications()
+      .pipe(
+        takeUntil(this.destroy$),
+        map((applications: AllApplications[]) => {
+          this.dataSource.data = applications;
+        }),
+        catchError((error: any) => {
+          console.error('Error occurred:', error);
+          return of(null);
+        }
+        )
+      )
+      .subscribe()
+  }
+  ngOnDestroy(): void {
+    this.destroy$.next();
+  }
+  
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
-    this.fetchAllApplications();
-  }
-  fetchAllApplications() {
-    this.getAllApplicationsService.getAllApplications().subscribe(
-      (applications: AllApplications[]) => {
-        this.dataSource.data = applications;
-      },
-      (error: any) => {
-        console.error('Error fetching applications:', error);
-      }
-    );
-  }
-
+  } 
+    
   onCellClick(row: AllApplications) {
-    console.log('Row clicked:', row);
-   
-  
+    console.log(row)
+    // const dialogRef = this.dialog.open(, {
+    //   data: row 
+    // });
 }
 }
 
