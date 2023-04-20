dd03545b2c7560d8911ab199cd45a2cf16182fe5
Saulegri <106665353+Saulegri@users.noreply.github.com>
2023-04-20 16:59:56 +0300

    feat:bugs

--- src/app/components/mortgage-application/mortgageAppComponents/personal-information/personal-information.component.ts
+++ src/app/components/mortgage-application/mortgageAppComponents/personal-information/personal-information.component.ts
@@ -1,4 +1,4 @@
-import { Component,EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
@@ -14,11 +14,11 @@ export class PersonalInformationComponent {
 
   constructor(private fb: FormBuilder) {
     this.personalInformationForm = this.fb.group({
-      name: new FormControl('', Validators.required),
-      lName: new FormControl('', Validators.required),
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
-      phoneNumber: new FormControl('',[Validators.required, Validators.pattern('\\+(9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|3[875]\\d|2[98654321]\\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\\d{1,14}$')]),
-      personalNumber: new FormControl('',[Validators.required, Validators.minLength(11), Validators.maxLength(11)])
+      phoneNumber: new FormControl('', [Validators.required, Validators.pattern('\\+(9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|3[875]\\d|2[98654321]\\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\\d{1,14}$')]),
+      personalNumber: new FormControl('', [Validators.required, Validators.minLength(11), Validators.maxLength(11)])
     });
 
     this.serviceSubscription = this.personalInformationForm.statusChanges.subscribe(() => {
