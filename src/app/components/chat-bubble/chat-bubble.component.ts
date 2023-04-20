895a40b3c236d1e09f87e0ffd6f9901209bf1288
[Valdemaras] <[valdemarlamm@gmail.com]>
2023-04-20 16:29:18 +0300

    Added basic bot with no context configuration

---
+++ src/app/components/chat-bubble/chat-bubble.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
+
+@Component({
+  selector: 'app-chat-bubble',
+  templateUrl: './chat-bubble.component.html',
+  styleUrls: ['./chat-bubble.component.css']
+})
+export class ChatBubbleComponent {
+  message = 'I am a little broken and need context configuration ask me anything like 2+2!';
+  isLoading = false;
+  isActive = true;
+  chatInput = '';
+
+  constructor(private http: HttpClient) {}
+
+  private apiUrl = 'https://api.openai.com/v1/chat/completions';
+  private apiKey = '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';
+
+  sendChatRequest(prompt: string, apiKey: string) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + apiKey
+    });
+
+    const body = {
+      'temperature': 0.5,
+      'max_tokens': 60,
+      'stop': " ",
+      'model': 'gpt-3.5-turbo',
+      "messages": [{"role": "assistant", "content": prompt}]
+    };
+
+    return this.http.post<any>(this.apiUrl, body, { headers: headers });
+  }
+
+  async sendMessage(message: string): Promise<void> {
+    try {
+      this.message = message;
+      this.isLoading = true;
+      this.isActive = true;
+      const response = await firstValueFrom(this.sendChatRequest(this.chatInput, this.apiKey));
+      console.log(response.choices);
+      console.log(response);
+      if (response.choices && response.choices.length > 0 && response.choices[0].message.content) {
+        this.message = response.choices[0].message.content;
+      } else {
+        this.message = 'Sorry, I could not understand your request.';
+      }
+    } catch (error) {
+      console.error(error);
+      this.message = 'Oops! Something went wrong.';
+    } finally {
+      this.isLoading = false;
+      this.chatInput = '';
+    }
+  }
+
+  onSubmit(): void {
+    const message = this.chatInput.trim();
+    if (message) {
+      this.sendMessage(message);
+    }
+  }
+}
