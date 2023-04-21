550716036b68e570c7694ac1ff446580df5de704
[Valdemaras] <[valdemarlamm@gmail.com]>
2023-04-21 14:06:01 +0300

    key get attemp

--- src/app/components/chat-bubble/chat-bubble.component.ts
+++ src/app/components/chat-bubble/chat-bubble.component.ts
@@ -22,7 +22,7 @@ export class ChatBubbleComponent {
   }
 
   getApiKey(){
-    this.http.get('/etc/secrets/API_KEY').subscribe({
+    this.http.get('/apiKey.env').subscribe({
       next: (response) => {
         try {
           console.log(response);
