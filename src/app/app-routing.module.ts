da29eae1cff8cd35e70814da28224950c0117fca
nikita <nikivavlt@gmail.com>
2023-04-21 17:01:15 +0300

    Feat/Fix: Fix router-outlet bug and create login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { ListOfApplicationsComponent } from './components/list-of-applications/list-of-applications.component';
 import { MainComponent } from './components/main/main.component';
+import { AuthorizationComponent } from './components/authorization/authorization.component';
 
 const routes: Routes = [
   {path: '', component: MainComponent},
+  {path: 'top-secret', component: AuthorizationComponent, pathMatch: "full"},
   {path: 'admin/applications', component: ListOfApplicationsComponent, pathMatch: "full"}
 ];
 
